99b5bcb2ca8f181718e02568099130433b2dec63
Michael Hesford <mhesford@cmslpc110.fnal.gov>
2023-02-06 13:23:06 -0600

    fixed errors

--- delta_phi.cc
+++ delta_phi.cc
@@ -4,8 +4,10 @@
 
 using namespace ROOT::VecOps;
 
-RVec<float> DeltaPhi(RVec<float> phi1, RVec<float> phi2){
-    // Calculate delta phi between two objects
-    float delta_phi = hardware::DeltaPhi(phi1[0],phi2[0]);
-    return {delta_phi};
+float DeltaPhi(RVec<float> phi1, RVec<float> phi2, RVec<int> idxs){
+    // Get the indices
+    int idx1 = idxs[0];
+    int idx2 = idxs[1];
+    float delta_phi = hardware::DeltaPhi(phi1[idx1],phi2[idx2]);
+    return delta_phi;
 }
