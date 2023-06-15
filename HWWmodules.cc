6c0edd99a5ed7f8190f34c27a535d19dad96dadc
Michael Hesford <mhesford@cmslpc166.fnal.gov>
2023-06-15 15:22:17 -0500

    added new functions

--- HWWmodules.cc
+++ HWWmodules.cc
@@ -34,37 +34,64 @@ RVec<int> PickDijets(RVec<float> eta, RVec<float> phi, RVec<float> mass) {
     }
 }
 
-int SignalLepton(RVec<float> lep_pt, RVec<float> lep_eta, RVec<float> lep_phi, RVec<float> lep_iso, float H_phi, float W_phi) {
-// function for lepton preselction (will expand/make separate for electrons/muons in future)
-    int leptonIdx = -1;
-    for (int il=0; il<lep_pt.size(); il++) {
-        if (lep_pt[il] > 25 && std::abs(lep_eta[il]) < 2.4 && lep_iso[il] < 0.2 && std::abs(hardware::DeltaPhi(lep_phi[il],W_phi)) < 2.5 && std::abs(hardware::DeltaPhi(lep_phi[il],W_phi)) > 1 && std::abs(hardware::DeltaPhi(lep_phi[il],H_phi)) > M_PI/2) {
-            leptonIdx = il;
+bool isLeptonPreselected(int nElectron, RVec<float> elePt, RVec<float> eleEta, RVec<float> eleIso, int nMuon, RVec<float> muPt, RVec<float> muEta, RVec<float> muIso){
+    if (nElectron < 1 && nMuon < 1) {return false;}
+    bool isLepPre = false;
+    for (int idx=0; idx<elePt.size(); idx++){
+        if (elePt[idx] > 10 && eleEta[idx] < 2.5 && eleIso[idx] < 0.5){
+            isLepPre = true;
+            return isLepPre;
+        }
+    }
+    for (int idx=0; idx<muPt.size(); idx++){
+        if (muPt[idx] > 10 && muEta[idx] < 2.4 && muIso[idx] < 0.5){
+            isLepPre = true;
+            break;
+        }
+    }   
+    return isLepPre;
+}
+
+int kinElectron(RVec<float> pt, RVec<float> eta, RVec<float> phi, float h_phi, float w_phi){
+    int eleIdx = -1;
+    for (int idx=0; idx<pt.size(); idx++) {
+        if (pt[idx] > 25 && std::abs(eta[idx]) < 2.5 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) < 2.5 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) > 1 && std::abs(hardware::DeltaPhi(phi[idx],h_phi)) > M_PI/2) {
+            eleIdx = idx;
             break;
         }
     }
-    return leptonIdx;
+    return eleIdx;
 }
 
-RVec<int> LeptonIdx(int electronIdx, int muonIdx, RVec<float> electron_pt, RVec<float> muon_pt) {
-// Decide whether an event is a lepton or muon type, collects lepton indices together
-    int e_index = -1;
-    int m_index = -1;
+int kinMuon(RVec<float> pt, RVec<float> eta, RVec<float> phi, float h_phi, float w_phi){
+    int muIdx = -1;
+    for (int idx=0; idx<pt.size(); idx++) {
+        if (pt[idx] > 25 && std::abs(eta[idx]) < 2.4 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) < 2.5 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) > 1 && std::abs(hardware::DeltaPhi(phi[idx],h_phi)) > M_PI/2) {
+            muIdx = idx;
+            break;
+        }
+    }
+    return muIdx;
+}
+
+int LeptonIdx(int electronIdx, int muonIdx, RVec<float> electron_pt, RVec<float> muon_pt) {
+// Decide whether an event is a lepton or muon type
+    int lepIdx = -1;
     if (electronIdx == -1) {
-        m_index = muonIdx;
+        lepIdx = 1; //its a muon
     }
     else if (muonIdx == -1) {
-        e_index = electronIdx;
+        lepIdx = 0; //its an electron
     }
     else {
         if (electron_pt[electronIdx] > muon_pt[muonIdx]) {
-            e_index = electronIdx;
+            lepIdx = 0;
         }
         else {
-            m_index = muonIdx;
+            lepIdx = 1;
         }
     }
-    return {e_index,m_index};
+    return lepIdx;
 }
 
 RVec<int> JetMasses(RVec<float> mass) { // Selects jets with pt > 50 GeV
@@ -130,16 +157,3 @@ float TransverseMass(float MET_pt, float obj_pt, float MET_phi, float obj_phi) {
     return sqrt(2.0*MET_pt*obj_pt*(1-cos(hardware::DeltaPhi(MET_phi,obj_phi))));
 }
 
-/*
-int GenSignalLepton(RVec<float> lep_pt, RVec<float> lep_eta, RVec<float> lep_phi, float H_phi, float W_phi) {
-//function for lepton preselction (will expand/make separate for electrons/muons in future)
-    RVec<int> leptonIdx = -1;
-    for (int il=0; il<lep_pt.size(); il++) {
-        if (lep_pt[il] > 25 && std::abs(lep_eta[il]) < 2.4 && std::abs(hardware::DeltaPhi(lep_phi[il],W_phi)) < 2.5 && std::abs(hardware::DeltaPhi(lep_phi[il],W_phi)) > 1 && std::abs(hardware::DeltaPhi(lep_phi[il],H_phi)) > M_PI/2) {
-            leptonIdx = il
-            break;
-        }
-    }
-     return leptonIdx;
-}
-*/
