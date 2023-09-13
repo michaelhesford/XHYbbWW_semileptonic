689f5adc61fa16495205b1230cf4848079d7dd69
Michael Hesford <mhesford@cmslpc156.fnal.gov>
2023-09-13 11:29:06 -0500

    new function

--- HWWmodules.cc
+++ HWWmodules.cc
@@ -20,7 +20,7 @@ RVec<int> PickDijets(RVec<float> eta, RVec<float> phi, RVec<float> mass) {
         return {-1, -1};
     }
     // now loop over the remaining jets,
-    for (int ijet=jet0Idx; ijet<eta.size(); ijet++) {
+    for (int ijet=jet0Idx+1; ijet<eta.size(); ijet++) {
         if (std::abs(eta[ijet]) < 2.4 && mass[ijet] > 50 && std::abs(hardware::DeltaPhi(phi[jet0Idx],phi[ijet])) > M_PI/2) {
             jet1Idx = ijet;
             break;
@@ -38,13 +38,13 @@ bool isLeptonPreselected(int nElectron, RVec<float> elePt, RVec<float> eleEta, R
     if (nElectron < 1 && nMuon < 1) {return false;}
     bool isLepPre = false;
     for (int idx=0; idx<elePt.size(); idx++){
-        if (elePt[idx] > 10 && eleEta[idx] < 2.5 && eleIso[idx] < 0.5){
+        if (elePt[idx] > 10 && std::abs(eleEta[idx]) < 2.5 && eleIso[idx] < 1){
             isLepPre = true;
             return isLepPre;
         }
     }
     for (int idx=0; idx<muPt.size(); idx++){
-        if (muPt[idx] > 10 && muEta[idx] < 2.4 && muIso[idx] < 0.5){
+        if (muPt[idx] > 10 && std::abs(muEta[idx]) < 2.4 && muIso[idx] < 1){
             isLepPre = true;
             break;
         }
@@ -52,10 +52,71 @@ bool isLeptonPreselected(int nElectron, RVec<float> elePt, RVec<float> eleEta, R
     return isLepPre;
 }
 
-int kinElectron(RVec<float> pt, RVec<float> eta, RVec<float> phi, float h_phi, float w_phi){
+bool isJetPreselected(int nFatJet, RVec<float> FatJet_pt, RVec<float> FatJet_eta, RVec<float> FatJet_mass, int nJet, RVec<float> Jet_pt, RVec<float> Jet_eta, bool ttCR) {
+    bool isJetPre = false;
+    if (ttCR == true) {
+        // perform jet pre-selection for ttbar control region
+        if (nFatJet < 1 || nJet < 1) {return false;}
+        bool ak4 = false;
+        for (int idx=0; idx<Jet_pt.size(); idx++) {
+            // check if ak4 jet exists
+            if (Jet_pt[idx] > 10 && std::abs(Jet_eta[idx]) < 2.4) {
+                break;
+                ak4 = true;
+            }
+        }
+        if (ak4 == false) {return false;}    
+        for (int idx=0; idx<FatJet_pt.size(); idx++) {
+            // check if ak8 jet exists
+            if (FatJet_pt[idx] > 200 && std::abs(FatJet_eta[idx]) < 2.4) {
+                return true;
+            }
+        }
+    }
+    else {
+        // perform standard jet pre-selection
+        if (nFatJet < 2) {return false;}
+        int idx1 = -1;
+        for (int idx=0; idx<FatJet_pt.size(); idx++) {
+            //check if first ak8 jets exists
+            if (FatJet_pt[idx] > 200 && std::abs(FatJet_eta[idx]) < 2.4 && FatJet_mass[idx] > 50) {
+                break;
+                idx1 = idx;
+            }
+            if (idx1 == -1) {return false;}
+            for (int idx=idx1+1; idx<FatJet_pt.size(); idx++) {
+                if (FatJet_pt[idx] > 200 && std::abs(FatJet_eta[idx]) < 2.4 && FatJet_mass[idx] > 50) {
+                    return true;
+                }
+            }
+        }
+    }
+    return isJetPre;
+}
+
+bool isLooseLeptonPreselected(int nElectron, RVec<float> elePt, RVec<float> eleEta, RVec<float> eleIso, int nMuon, RVec<float> muPt, RVec<float> muEta, RVec<float> muIso){
+    if (nElectron < 1 && nMuon < 1) {return false;}
+    bool isLepPre = false;
+    for (int idx=0; idx<elePt.size(); idx++){
+        if (elePt[idx] > 10 && eleEta[idx] < 2.5 && eleIso[idx] < 1){
+            isLepPre = true;
+            return isLepPre;
+        }
+    }
+    for (int idx=0; idx<muPt.size(); idx++){
+        if (muPt[idx] > 10 && muEta[idx] < 2.4 && muIso[idx] < 1){
+            isLepPre = true;
+            break;
+        }
+    }
+    return isLepPre;
+}
+
+
+int kinElectron(RVec<float> pt, RVec<float> eta, RVec<float> iso){
     int eleIdx = -1;
     for (int idx=0; idx<pt.size(); idx++) {
-        if (pt[idx] > 25 && std::abs(eta[idx]) < 2.5 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) < 2.5 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) > 1 && std::abs(hardware::DeltaPhi(phi[idx],h_phi)) > M_PI/2) {
+        if (pt[idx] > 25 && std::abs(eta[idx]) < 2.5 && iso[idx] < 0.1) {
             eleIdx = idx;
             break;
         }
@@ -63,10 +124,10 @@ int kinElectron(RVec<float> pt, RVec<float> eta, RVec<float> phi, float h_phi, f
     return eleIdx;
 }
 
-int kinMuon(RVec<float> pt, RVec<float> eta, RVec<float> phi, float h_phi, float w_phi){
+int kinMuon(RVec<float> pt, RVec<float> eta, RVec<float> iso){
     int muIdx = -1;
     for (int idx=0; idx<pt.size(); idx++) {
-        if (pt[idx] > 25 && std::abs(eta[idx]) < 2.4 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) < 2.5 && std::abs(hardware::DeltaPhi(phi[idx],w_phi)) > 1 && std::abs(hardware::DeltaPhi(phi[idx],h_phi)) > M_PI/2) {
+        if (pt[idx] > 25 && std::abs(eta[idx]) < 2.4 && iso[idx] < 0.1) {
             muIdx = idx;
             break;
         }
@@ -74,6 +135,26 @@ int kinMuon(RVec<float> pt, RVec<float> eta, RVec<float> phi, float h_phi, float
     return muIdx;
 }
 
+int leptonType_studies(RVec<float> electron_pt, RVec<float> muon_pt) {
+    if (electron_pt.size() == 0) {
+        return 1;
+    }
+    else if (muon_pt.size() == 0) {
+        return 0;
+    }
+    else if (muon_pt[0] > electron_pt[0]) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+/*
+int kinElectronStudies
+
+int kinMuonStudies
+*/
 int LeptonIdx(int electronIdx, int muonIdx, RVec<float> electron_pt, RVec<float> muon_pt) {
 // Decide whether an event is a lepton or muon type
     int lepIdx = -1;
