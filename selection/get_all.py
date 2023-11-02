b4c7f00d4bb2192d4b952f52b72e313156a4275c
Michael Hesford <mhesford@cmslpc166.fnal.gov>
2023-11-02 13:00:18 -0500

    script to reorganize template hists

--- selection/get_all.py
+++ selection/get_all.py
@@ -25,15 +25,27 @@ def adjust_negative_bins(hist):
 
 def organize_bkg_hists(backgrounds):
     #makes fully hadded histograms for each systematic+year combo as well as a fully nominal histogram, stored in a new rootfile for each setname (format: XHYbbWWselection_<SETNAME>.root)
+    #EX: hist 'MXvMY_SR_pass__pileup_16_up' is the sum of the pileup up histo from 2016 + nominal histogram from all other years
     for setname in backgrounds:
         outFile = ROOT.TFile('selection/XHYbbWWselection_{}.root'.format(setname),'UPDATE')
         for region in ['pass_SR','fail_SR','pass_ttCR','fail_ttCR']:
             systs = {
-                '16APV' : ['L1PreFiringWeight_up','L1PreFiringWeight_down','Pileup_up','Pileup_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down'],
-                '16' : ['L1PreFiringWeight_up','L1PreFiringWeight_down','Pileup_up','Pileup_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down'],
-                '17' : ['L1PreFiringWeight_up','L1PreFiringWeight_down','Pileup_up','Pileup_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down'],
-                '18' : ['Pileup_up','Pileup_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down']
+                '16APV' : ['L1PreFiringWeight_up','L1PreFiringWeight_down','Pdfweight_up','Pdfweight_down','Pileup_up','Pileup_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down'],
+                '16' : ['L1PreFiringWeight_up','L1PreFiringWeight_down','Pdfweight_up','Pdfweight_down','Pileup_up','Pileup_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down'],
+                '17' : ['L1PreFiringWeight_up','L1PreFiringWeight_down','Pdfweight_up','Pdfweight_down','Pileup_up','Pileup_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down'],
+                '18' : ['Pileup_up','Pileup_down','Pdfweight_up','Pdfweight_down','JES_up','JES_down','JMS_up','JMS_down','JER_up','JER_down','JMR_up','JMR_down']
             }
+            '''
+            if 'ttbar' in setname:
+                systs['16APV'].append('TptReweight_up')
+                systs['16APV'].append('TptReweight_down')
+                systs['16'].append('TptReweight_up')
+                systs['16'].append('TptReweight_down')
+                systs['17'].append('TptReweight_up')
+                systs['17'].append('TptReweight_down')
+                systs['18'].append('TptReweight_up')
+                systs['18'].append('TptReweight_down')
+            '''
             nom_histgroup = HistGroup('MXvMY_{}__nominal'.format(region))
             for year in systs.keys():
                 nom_years = systs.keys()
@@ -65,7 +77,7 @@ def organize_bkg_hists(backgrounds):
                         sys_path = 'selection/raw_selection/XHYbbWWselection_{}_{}_{}.root'.format(setname,year,sys)
                         sys_hist = 'MXvMY_{}__nominal'.format(region)
                     sysFile = ROOT.TFile.Open(sys_path,'READ')
-                    sysHist = inFile.Get(sys_hist)
+                    sysHist = sysFile.Get(sys_hist)
                     sysHist.SetDirectory(0)
                     sys_histgroup.Add(sysHist.GetName()+'_'+year,sysHist)
                     #next add to the histgroup the corresponding nominal histograms from all other years
@@ -79,6 +91,22 @@ def organize_bkg_hists(backgrounds):
                     adjust_negative_bins(merged_hist)
                     outFile.cd()
                     merged_hist.Write() #write each systematic histogram to the out file 
+            if 'ttbar' in setname:
+                #Apply the top pt reweight as a single correction for all the years
+                tpt_systs = ['TptReweight_up','TptReweight_down']
+                for tpt in tpt_systs:
+                    tpt_histgroup = HistGroup('MXvMY_{}__{}'.format(region,tpt)) #Ex: MXvMY_fail_SR__TptReweight_up                
+                    for year in systs.keys():
+                        tpt_path = 'selection/raw_selection/XHYbbWWselection_{}_{}.root'.format(setname,year)
+                        tpt_hist = 'MXvMY_{}__{}'.format(region,tpt)
+                        tptFile = ROOT.TFile.Open(tpt_path,'READ')
+                        tptHist = tptFile.Get(tpt_hist)
+                        tptHist.SetDirectory(0)
+                        tpt_histgroup.Add(tptHist.GetName()+'_'+year,tptHist)
+                    tpt_hist_merged = tpt_histgroup.Merge()
+                    adjust_negative_bins(tpt_hist_merged)
+                    outFile.cd()
+                    tpt_hist_merged.Write() #write the tpt histogram to the out file
             merged_nom_hist = nom_histgroup.Merge()
             adjust_negative_bins(merged_nom_hist)
             outFile.cd()
@@ -89,19 +117,23 @@ def MakeRun2():
     ExecuteCmd(cmd)
 
 if __name__ == '__main__':
-    '''    
+    
     redirector = 'root://cmseos.fnal.gov/'
-    eos_path = '/store/user/mhesford/XHYbbWW_semileptonic/selection/'
-
+    #eos_path = '/store/user/mhesford/XHYbbWW_semileptonic/selection/'
+    #eos_path = '/store/user/mhesford/XHYbbWW_semileptonic/selection_HF/'
+    #eos_path = '/store/user/mhesford/XHYbbWW_semileptonic/selection_dPhi/'
+    eos_path = '/store/user/mhesford/XHYbbWW_semileptonic/selection_combined/'
+ 
     cmd = 'eos {} ls {}XHYbbWWselection*'.format(redirector,eos_path)
-
+    
     rawFiles = subprocess.check_output(cmd, shell=True)
     files = rawFiles.split('\n')
     for fName in files:
         ExecuteCmd('xrdcp {}{}{} selection/raw_selection/'.format(redirector, eos_path, fName))
-    ''' 
-    #MakeRun2()
-    #combine_ttbar()
-    #organize_bkg_hists(['XHY-2000-1000','ttbar','WWto1L1Nu2Q','WZto1L1Nu2Q'])
-    organize_bkg_hists(['XHY-1000-500'])
 
+    combine_ttbar()
+        
+    MakeRun2()
+
+    #organize_bkg_hists(['XHY-4000-2500','XHY-3500,1800','XHY-3000-1400','XHY-2000-1000','XHY-1000-500','ttbar'])
+    organize_bkg_hists(['XHY-3000-1400','ttbar'])
