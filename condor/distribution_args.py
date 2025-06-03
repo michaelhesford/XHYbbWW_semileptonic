cba1fcfaafffcc2db79a977c2fe90541a14c59e3
Michael Hesford <mhesford@cmslpc239.fnal.gov>
2025-06-03 14:00:47 -0500

    updates

--- condor/distribution_args.py
+++ condor/distribution_args.py
@@ -2,42 +2,18 @@ import glob,os
 
 out = open('condor/distribution_args.txt','w')
 
-for t in ['Signal','Background']:
-    if t == 'Signal':
-        for f in glob.glob('raw_nano/Signal/*'):
-            if os.path.getsize(f) == 0:
-                print ('File %s is empty... Skipping.'%(f))
-                continue
-
-            # filename will look like: raw_nano/Signal/setname_year.txt
-            filename = f.split('/')[-1].split('.')[0]
-            nfiles = len(open(f,'r').readlines())
-            setname = filename.split('_')[0]
-            year = filename.split('_')[1]
-
-            njobs = int(nfiles/2)
-            if njobs == 0:	# this occurs when nfiles = 1
-	        njobs += 1
-            for i in range(1,njobs+1):
-  	        out.write('-s %s -y %s -j %s -n %s \n'%(setname,year,i,njobs))
-
-    elif t == 'Background': # Maybe i can figure out a way to do this in one loop...
-        for d in glob.glob('raw_nano/Background/*'):
-            for f in glob.glob(d+'/*'):
-                if os.path.getsize(f) == 0:
-                    print ('File %s is empty... Skipping.'%(f))
-                    continue
-
-                # filename will look like: raw_nano/Background/setname_year.txt
-                filename = f.split('/')[-1].split('.')[0]
-                nfiles = len(open(f,'r').readlines())
-                setname = filename.split('_')[0]
-                year = filename.split('_')[1]
-
-                njobs = int(nfiles/2)
-                if njobs == 0:      # this occurs when nfiles = 1
-                    njobs += 1
-                for i in range(1,njobs+1):
-                    out.write('-s %s -y %s -j %s -n %s \n'%(setname,year,i,njobs))
+for f in glob.glob('snapshots/*snapshot.txt'):
+    if os.path.getsize(f) == 0 or f.split('/')[-1].startswith('Data'):
+        print ('File %s is empty or JetHT data... Skipping.'%(f))
+        continue
+    signals = ['XHY-4000-2500','XHY-3500-1800','XHY-3000-1400','XHY-2000-1000','XHY-1600-700','XHY-1000-500']
+    # filename will look like: snapshots/setname_year_snapshot.txt
+    filename = f.split('/')[-1].split('.')[0]
+    nfiles = len(open(f,'r').readlines())
+    setname = filename.split('_')[0]
+    if 'XHY' in setname and setname not in signals:
+        continue
+    year = filename.split('_')[1]
+    out.write('-s %s -y %s -j 1 -n 1\n'%(setname,year))
 
 out.close()
--- condor/run_distributions.sh
+++ condor/run_distributions.sh
@@ -2,21 +2,34 @@
 echo "Run script starting"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 xrdcp root://cmseos.fnal.gov//store/user/mhesford/XHYbbWW_semileptonic.tgz ./
-export SCRAM_ARCH=slc7_amd64_gcc820
-scramv1 project CMSSW CMSSW_11_1_4
+export SCRAM_ARCH=el8_amd64_gcc10
+scramv1 project CMSSW CMSSW_12_3_5
 tar -xzvf XHYbbWW_semileptonic.tgz
 rm XHYbbWW_semileptonic.tgz
-#rm *.root
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir/
 tar -xzf tarball.tgz; rm tarball.tgz
-cp -r * ../CMSSW_11_1_4/src/semileptonic; cd ../CMSSW_11_1_4/src/
+cp -r * ../CMSSW_12_3_5/src/semileptonic/; cd ../CMSSW_12_3_5/src/
 echo 'IN RELEASE'
 pwd
 ls
 eval `scramv1 runtime -sh`
 rm -rf timber-env
-python -m virtualenv timber-env
+python3 -m venv timber-env
+
+cat <<EOT >> timber-env/bin/activate
+
+export BOOSTPATH=/cvmfs/cms.cern.ch/el8_amd64_gcc10/external/boost/1.78.0-0d68c45b1e2660f9d21f29f6d0dbe0a0/lib
+export LIBPATH=/cvmfs/cms.cern.ch/el8_amd64_gcc10/external/python3/3.9.6-67e5cf5b4952101922f1d4c8474baa39/lib/
+if grep -q '\${BOOSTPATH}' <<< '\${LD_LIBRARY_PATH}'
+then
+  echo 'BOOSTPATH already on LD_LIBRARY_PATH'
+else
+  export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:\${LIBPATH}:\${BOOSTPATH}
+  echo 'BOOSTPATH added to PATH'
+fi
+EOT
+
 source timber-env/bin/activate
 cd TIMBER
 source setup.sh
@@ -25,4 +38,5 @@ cd ../semileptonic
 echo python PlotDistributions.py $*
 python PlotDistributions.py $*
 
-xrdcp -f *.root root://cmseos.fnal.gov//store/user/mhesford/XHYbbWW_semileptonic/distributions/
+
+xrdcp -f *distributions.root root://cmseos.fnal.gov//store/user/mhesford/XHYbbWW_semileptonic/plots/distributions/
